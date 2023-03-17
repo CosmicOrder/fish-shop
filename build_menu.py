b6da6a85e3ba13cccc05a6ff4f65458c23ba3d56
CosmicOrder <Matrixxturn@yandex.ru>
2023-03-18 00:27:21 +0300

    refactor: change variable name

--- build_menu.py
+++ build_menu.py
@@ -7,7 +7,7 @@ def built_menu(
         n_cols: int,
         footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None,
 ):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    menu = [buttons[start_slice:start_slice + n_cols] for start_slice in range(0, len(buttons), n_cols)]
     if footer_buttons:
         menu.append(footer_buttons if isinstance(footer_buttons, list) else [
             footer_buttons])
